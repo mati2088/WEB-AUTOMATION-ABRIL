7da04e9e5b292a6c6da6683d0c70cb97fccb5892
matias-arana_cvideo <matias.arana@nubiral.onmicrosoft.com>
2026-05-08 20:31:04 -0300

    Se agrego variables de entorno, se configuro report allure, se agrego data driven

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,16 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+   reporter: [
+    ["html"],            // reporte local de Playwright (para debug)
+  
+  ["allure-playwright", {
+      detail: true,
+      outputFolder: "allure-results",  // carpeta con resultados raw
+      suiteTitle: false,
+    }],  ],
+  // ... resto de la config
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -31,7 +40,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     video: 'retain-on-failure',
-    screenshot: 'only-on-failure',
+    screenshot: 'on',
   },
 
   
--- src/tests/tarjetasVirtuales.spec.ts
+++ src/tests/tarjetasVirtuales.spec.ts
@@ -2,21 +2,28 @@ import { test, expect,Page } from '@playwright/test';
 import {LoginSteps} from "../steps/LoginSteps"
 import { MenuPrincipalSteps } from '../steps/MenuPrincipalSteps';
 
+import {UsuarioValido} from "../data/users.json"
+
+import 'dotenv/config' 
+
+
+const url = process.env.URL_BANKING ?? ''
+
+
 let page:Page;
 
 test.beforeAll(async ({browser})=>{
     const context = await browser.newContext()
     page=await context.newPage()
     const loginSteps = new LoginSteps(page)
-    await loginSteps.navegarWeb("https://homebanking-demo-tests.netlify.app/");
-    await loginSteps.IniciarSesion("demo","demo123");
+    await loginSteps.navegarWeb(url);
+    await loginSteps.IniciarSesion(UsuarioValido.usuario,UsuarioValido.password);
 })
 
 test('Generar Tarjeta Exitosamente', async () => {
    
     const menuPrincipalSteps=new MenuPrincipalSteps(page)
     await menuPrincipalSteps.generarTarjetaVirtual()
-    await menuPrincipalSteps.waitFor(10000);
 })
 
 
